0ce770aea62e9bdfc18f62747979e72872b96c61
Rolland Campbell <rollandcampbell@msn.com>
2020-04-02 11:21:28 -0600

    paypal buttons showing, working good

--- clientsrc/src/components/eventCards.vue
+++ clientsrc/src/components/eventCards.vue
@@ -32,14 +32,8 @@
         <u>Website:</u>&ensp;
         <a class="webLink" :href="'https://'+ this.eventProps.link">{{this.eventProps.link}}</a>
       </h5>
-      <a
-        class="webLink"
-        data-toggle="collapse"
-        :href="'#collapseExample'+ this.eventProps._id"
-        role="button"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-      >
+      <a class="webLink" data-toggle="collapse" :href="'#collapseExample'+ this.eventProps._id" role="button"
+        aria-expanded="false" aria-controls="collapseExample">
         <h5>
           <u>Event Information:</u>&ensp;
           <span class="smallText">(Click to open)</span>
@@ -50,15 +44,8 @@
           <h5>{{this.eventProps.description}}</h5>
         </div>
       </div>
-      <div
-        class="buttonRow"
-        v-if="$auth.isAuthenticated && $auth.userInfo.app_metadata.role == 'admin'"
-      >
-        <button
-          class="btn btnSm btn-warning"
-          data-toggle="modal"
-          :data-target="'#editModal'+ eventProps.id"
-        >
+      <div class="buttonRow" v-if="$auth.isAuthenticated && $auth.userInfo.app_metadata.role == 'admin'">
+        <button class="btn btnSm btn-warning" data-toggle="modal" :data-target="'#editModal'+ eventProps.id">
           <i class="fa fa-pencil" aria-hidden="true"></i>
         </button>
         <eventEditModal :id="'editModal'+eventProps.id" :edit="eventProps" />
@@ -109,156 +96,167 @@
       <div class="closeButton">
         <button class="btn btn-secondary" @click="closeAll">X</button>
       </div>
-      <div :id="'paypal' + this.eventProps._id"></div>
+      <div class="paypalButtons" :id="'paypal' + this.eventProps._id"></div>
     </div>
   </div>
 </template>
 <script>
-import EventEditModal from "../components/editEventModal";
-export default {
-  name: "EventCards",
-  props: ["eventProps"],
-  data() {
-    return {
-      visibleReg: false,
-      visiblePay: false,
-      athlete: {
-        eventTitle: this.eventProps.title,
-        eventDate: this.eventProps.eventDate
-      }
-    };
-  },
-  mounted() {
-    let vm = this;
-    paypal
-      .Buttons({
-        createOrder(data, actions) {
-          // This function sets up the details of the transaction, including the amount and line item details.
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: vm.eventProps.cost
-                }
-              }
-            ]
-          });
-        },
-        onApprove: function(data, actions) {
-          // This function captures the funds from the transaction.
-          return actions.order.capture().then(function(details) {
-            // This function shows a transaction success message to your buyer.
-            alert("Transaction completed by " + details.payer.name.given_name);
-          });
+  import EventEditModal from "../components/editEventModal";
+  export default {
+    name: "EventCards",
+    props: ["eventProps"],
+    data() {
+      return {
+        visibleReg: false,
+        visiblePay: false,
+        athlete: {
+          eventTitle: this.eventProps.title,
+          eventDate: this.eventProps.eventDate
         }
-      })
-      .render("#paypal" + this.eventProps._id);
-    //This function displays Smart Payment Buttons on your web page.
-  },
-  methods: {
-    register() {
-      this.visibleReg = true;
+      };
     },
+    mounted() {
+      let vm = this;
+      paypal
+        .Buttons({
+          createOrder(data, actions) {
+            // This function sets up the details of the transaction, including the amount and line item details.
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: vm.eventProps.cost
+                  }
+                }
+              ]
+            });
+          },
+          onApprove(data, actions) {
+            // This function captures the funds from the transaction.
+            return actions.order.capture().then(function (details) {
+              // This function shows a transaction success message to your buyer.
+              vm.registerAthlete();
+              alert("Transaction completed by " + details.payer.name.given_name);
+              vm.closeAll();
+            });
+          }
+        })
+        .render("#paypal" + this.eventProps._id);
+      //This function displays Smart Payment Buttons on your web page.
+    },
+    methods: {
+      register() {
+        this.visibleReg = true;
+      },
 
-    showPay() {
-      this.visiblePay = true;
-    },
+      showPay() {
+        this.visiblePay = true;
+      },
 
-    deleteEvent() {
-      this.$store.dispatch("deleteEvent", this.eventProps);
+      deleteEvent() {
+        this.$store.dispatch("deleteEvent", this.eventProps);
+      },
+      closeButton() {
+        this.visibleReg = false;
+      },
+      closeAll() {
+        this.visiblePay = false;
+        this.visibleReg = false;
+      },
+      registerAthlete() {
+        this.$store.dispatch("registerAthlete", this.athlete)
+      }
     },
-    closeButton() {
-      this.visibleReg = false;
-    },
-    closeAll() {
-      this.visiblePay = false;
-      this.visibleReg = false;
+    components: {
+      EventEditModal
     }
-  },
-  components: {
-    EventEditModal
-  }
-};
+  };
 </script>
 <style>
-.cardStyle {
-  height: 40vh;
-  margin-bottom: 2vh;
-  display: flex;
-}
+  .cardStyle {
+    height: 40vh;
+    margin-bottom: 2vh;
+    display: flex;
+  }
 
-.imgStyle {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(255, 255, 255, 0.644);
-  border-top-left-radius: 20px;
-  border-bottom-left-radius: 20px;
-}
+  .imgStyle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.644);
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+  }
 
-.eventInfo {
-  display: grid;
-  align-content: center;
-  background-color: rgba(255, 255, 255, 0.644);
-  border-top-right-radius: 20px;
-  border-bottom-right-radius: 20px;
-  text-shadow: 1px 1px white;
-  overflow-y: hidden;
-}
+  .eventInfo {
+    display: grid;
+    align-content: center;
+    background-color: rgba(255, 255, 255, 0.644);
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+    text-shadow: 1px 1px white;
+    overflow-y: hidden;
+  }
 
-.registerStyle {
-  padding: 20px;
-  display: grid;
-  align-content: center;
-  background-color: rgba(255, 255, 255, 0.644);
-  border-radius: 20px;
-  text-shadow: 1px 1px white;
-}
+  .registerStyle {
+    padding: 20px;
+    display: grid;
+    align-content: center;
+    background-color: rgba(255, 255, 255, 0.644);
+    border-radius: 20px;
+    text-shadow: 1px 1px white;
+  }
 
-.payStyle {
-  align-content: center;
-  background-color: rgba(255, 255, 255, 0.644);
-  border-radius: 20px;
-}
+  .payStyle {
+    align-content: center;
+    background-color: rgba(255, 255, 255, 0.644);
+    border-radius: 20px;
+  }
 
-img {
-  height: 90%;
-  width: 90%;
-  border-radius: 20px;
-}
+  img {
+    height: 90%;
+    width: 90%;
+    border-radius: 20px;
+  }
 
-.webLink {
-  color: black;
-}
+  .webLink {
+    color: black;
+  }
 
-.btn.fab {
-  padding: 1rem;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+  .btn.fab {
+    padding: 1rem;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-.registerButton {
-  position: absolute;
-  right: 1rem;
-  top: 42%;
-}
+  .registerButton {
+    position: absolute;
+    right: 1rem;
+    top: 42%;
+  }
 
-.closeButton {
-  position: absolute;
-  right: 1rem;
-  top: 1rem;
-}
+  .closeButton {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+  }
 
-.buttonRow {
-  display: flex;
-  justify-content: space-evenly;
-}
+  .buttonRow {
+    display: flex;
+    justify-content: space-evenly;
+  }
 
-.btnSm {
-  border-radius: 50%;
-  border-color: black;
-  border-style: solid;
-}
+  .btnSm {
+    border-radius: 50%;
+    border-color: black;
+    border-style: solid;
+  }
+
+  .paypalButtons {
+    position: relative;
+    top: 13vh;
+    /* left: 2vw; */
+  }
 </style>
