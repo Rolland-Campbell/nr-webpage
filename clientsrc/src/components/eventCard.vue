5b85c2598254a34b65269adea4e8729406293838
Rolland-Campbell <rollandcampbell@msn.com>
2020-04-02 08:42:14 -0600

    button fix and font fix

--- clientsrc/src/components/eventCard.vue
+++ clientsrc/src/components/eventCard.vue
@@ -5,51 +5,64 @@
         <img :src="this.eventProps.logo" alt="logo" />
       </div>
       <div class="card-body">
-        <h5 class="card-title">{{this.eventProps.title}}</h5>
+        <p class="card-title">{{this.eventProps.title}}</p>
         <hr />
-        <h5>
+        <p>
           <u>Date:</u>
           {{this.eventProps.eventDate | date}}
-        </h5>
-        <h5>
+        </p>
+        <p>
           <u>Time:</u>
           {{this.eventProps.eventTime}}
-        </h5>
-        <h5>
+        </p>
+        <p>
           <u>Host:</u>
           {{this.eventProps.host}}
-        </h5>
-        <h5>
+        </p>
+        <p>
           <u>Registration Cost:</u>
           ${{this.eventProps.cost}}
-        </h5>
-        <h5>
+        </p>
+        <p>
           <u>Location:</u>
           {{this.eventProps.location}}
-        </h5>
-        <h5>
+        </p>
+        <p>
           <u>Website:</u>&ensp;
           <a class="webLink" :href="'https://'+ this.eventProps.link">{{this.eventProps.link}}</a>
-        </h5>
+        </p>
         <p>
-          <a class="linkInfo" data-toggle="collapse" :href="'#collapseExample'+ this.eventProps._id" role="button"
-            aria-expanded="false" aria-controls="collapseExample">
-            <h5>
+          <a
+            class="linkInfo"
+            data-toggle="collapse"
+            :href="'#collapseExample'+ this.eventProps._id"
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            <p>
               <u>Event Information:</u>&ensp;
               <span class="smallText">(Click to open)</span>
-            </h5>
+            </p>
           </a>
         </p>
         <div class="collapse" :id="'collapseExample'+ this.eventProps._id">
           <div class="card card-body mb-3">
-            <h5>{{this.eventProps.description}}</h5>
+            <p>{{this.eventProps.description}}</p>
           </div>
         </div>
         <div class="regButton">
           <RegModal :eventProps="eventProps" :id="eventProps._id" />
         </div>
-        <div class="buttonRow" v-if="$auth.isAuthenticated && $auth.userInfo.app_metadata.role == 'admin'">
-          <button class="btn btnSm btn-warning" data-toggle="modal" :data-target="'#editModal'+ eventProps.id">
+        <div
+          class="buttonRow"
+          v-if="$auth.isAuthenticated && $auth.userInfo.app_metadata.role == 'admin'"
+        >
+          <button
+            class="btn btnSm btn-warning"
+            data-toggle="modal"
+            :data-target="'#editModal'+ eventProps.id"
+          >
             <i class="fa fa-pencil" aria-hidden="true"></i>
           </button>
           <eventEditModal :id="'editModal'+eventProps.id" :edit="eventProps" />
@@ -64,83 +77,83 @@
 
 
 <script>
-  import RegModal from "../components/registrationModal";
-  import EventEditModal from "../components/editEventModal";
-  export default {
-    name: "EventCard",
-    props: ["eventProps"],
-    data() {
-      return {};
-    },
-    mounted() {
-      return this.$store.state.events;
-    },
-    computed: {},
-    methods: {
-      deleteEvent() {
-        this.$store.dispatch("deleteEvent", this.eventProps);
-      }
-    },
-    components: {
-      EventEditModal,
-      RegModal
+import RegModal from "../components/registrationModal";
+import EventEditModal from "../components/editEventModal";
+export default {
+  name: "EventCard",
+  props: ["eventProps"],
+  data() {
+    return {};
+  },
+  mounted() {
+    return this.$store.state.events;
+  },
+  computed: {},
+  methods: {
+    deleteEvent() {
+      this.$store.dispatch("deleteEvent", this.eventProps);
     }
-  };
+  },
+  components: {
+    EventEditModal,
+    RegModal
+  }
+};
 </script>
 
 
 <style scoped>
-  .card {
-    background-color: rgba(7, 5, 5, 0.616);
-    border-color: black;
-    border-style: solid;
-  }
+.card {
+  background-color: rgba(7, 5, 5, 0.616);
+  border-color: black;
+  border-style: solid;
+}
 
-  .card-body {
-    text-align: left;
-  }
+.card-body {
+  text-align: left;
+}
 
-  .card-title {
-    text-align: center;
-  }
+.card-title {
+  text-align: center;
+}
 
-  .btnSm {
-    border-radius: 50%;
-    border-color: black;
-    border-style: solid;
-  }
+.btnSm {
+  border-radius: 50%;
+  border-color: black;
+  border-style: solid;
+}
 
-  .regButton {
-    display: flex;
-    justify-content: center;
-  }
+.regButton {
+  display: flex;
+  justify-content: center;
+}
 
-  .buttonRow {
-    margin-top: 2vh;
-    display: flex;
-    justify-content: space-evenly;
-  }
+.buttonRow {
+  margin-top: 2vh;
+  display: flex;
+  justify-content: space-evenly;
+}
 
-  h5 {
-    padding-bottom: 1vh;
-  }
+h5 {
+  padding-bottom: 1vh;
+}
 
-  img {
-    height: 20vh;
-    width: 20vh;
-    border-radius: 15px;
-    justify-content: center;
-  }
+img {
+  height: 20vh;
+  width: 20vh;
+  border-radius: 15px;
+  justify-content: center;
+}
 
-  .linkInfo {
-    color: black;
-  }
+.linkInfo {
+  color: black;
+}
 
-  .smallText {
-    font-size: small;
-  }
+.smallText {
+  font-size: small;
+}
 
-  .webLink {
-    color: black;
-  }
+.webLink {
+  color: black;
+}
 </style>
--- clientsrc/src/components/eventCards.vue
+++ clientsrc/src/components/eventCards.vue
@@ -1,34 +1,64 @@
 <template>
   <div class="cardStyle col-12">
     <div class="col-2 imgStyle">
-      <img :src="this.eventProps.logo" alt="logo">
+      <img :src="this.eventProps.logo" alt="logo" />
     </div>
     <div class="col-4 eventInfo">
       <div class="registerButton">
         <button class="btn btn-info fab" @click="register">Register</button>
       </div>
-      <h3>{{this.eventProps.title}}</h3>
-      <h3><u>Date</u>: {{this.eventProps.eventDate | date}}</h3>
-      <h3><u>Time</u>: {{this.eventProps.eventTime}}</h3>
-      <h3><u>Host</u>: {{this.eventProps.host}}</h3>
-      <h3><u>Registration Cost:</u> ${{this.eventProps.cost}}</h3>
-      <h3><u>Location:</u> {{this.eventProps.location}}</h3>
-      <h3><u>Website:</u>&ensp; <a class="webLink" :href="'https://'+ this.eventProps.link">{{this.eventProps.link}}</a>
-      </h3>
-      <a class="webLink" data-toggle="collapse" :href="'#collapseExample'+ this.eventProps._id" role="button"
-        aria-expanded="false" aria-controls="collapseExample">
-        <h3>
+      <h5>{{this.eventProps.title}}</h5>
+      <h5>
+        <u>Date</u>
+        : {{this.eventProps.eventDate | date}}
+      </h5>
+      <h5>
+        <u>Time</u>
+        : {{this.eventProps.eventTime}}
+      </h5>
+      <h5>
+        <u>Host</u>
+        : {{this.eventProps.host}}
+      </h5>
+      <h5>
+        <u>Registration Cost:</u>
+        ${{this.eventProps.cost}}
+      </h5>
+      <h5>
+        <u>Location:</u>
+        {{this.eventProps.location}}
+      </h5>
+      <h5>
+        <u>Website:</u>&ensp;
+        <a class="webLink" :href="'https://'+ this.eventProps.link">{{this.eventProps.link}}</a>
+      </h5>
+      <a
+        class="webLink"
+        data-toggle="collapse"
+        :href="'#collapseExample'+ this.eventProps._id"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseExample"
+      >
+        <h5>
           <u>Event Information:</u>&ensp;
           <span class="smallText">(Click to open)</span>
-        </h3>
+        </h5>
       </a>
       <div class="collapse" :id="'collapseExample'+ this.eventProps._id">
         <div class="card card-body mb-3" tabindex="2">
           <h5>{{this.eventProps.description}}</h5>
         </div>
       </div>
-      <div class="buttonRow" v-if="$auth.isAuthenticated && $auth.userInfo.app_metadata.role == 'admin'">
-        <button class="btn btnSm btn-warning" data-toggle="modal" :data-target="'#editModal'+ eventProps.id">
+      <div
+        class="buttonRow"
+        v-if="$auth.isAuthenticated && $auth.userInfo.app_metadata.role == 'admin'"
+      >
+        <button
+          class="btn btnSm btn-warning"
+          data-toggle="modal"
+          :data-target="'#editModal'+ eventProps.id"
+        >
           <i class="fa fa-pencil" aria-hidden="true"></i>
         </button>
         <eventEditModal :id="'editModal'+eventProps.id" :edit="eventProps" />
@@ -75,7 +105,7 @@
       <button class="btn btn-info" @click="showPay">Next</button>
     </div>
     <!-- pay tab -->
-    <div class="col-2 payStyle ml-2 " v-if="visiblePay == true">
+    <div class="col-2 payStyle ml-2" v-if="visiblePay == true">
       <div class="closeButton">
         <button class="btn btn-secondary" @click="closeAll">X</button>
       </div>
@@ -84,150 +114,150 @@
   </div>
 </template>
 <script>
-  import EventEditModal from "../components/editEventModal";
-  export default {
-    name: 'EventCards',
-    props: ["eventProps"],
-    data() {
-      return {
-        visibleReg: false,
-        visiblePay: false,
-        athlete: {
-          eventTitle: this.eventProps.title,
-          eventDate: this.eventProps.eventDate
-        },
-      };
-    },
-    mounted() {
-      paypal
-        .Buttons({
-          createOrder: function (data, actions) {
-            // This function sets up the details of the transaction, including the amount and line item details.
-            return actions.order.create({
-              purchase_units: [
-                {
-                  amount: {
-                    value: 20
-                  }
-                }
-              ]
-            });
-          },
-          onApprove: function (data, actions) {
-            // This function captures the funds from the transaction.
-            return actions.order.capture().then(function (details) {
-              // This function shows a transaction success message to your buyer.
-              alert("Transaction completed by " + details.payer.name.given_name);
-            });
-          }
-        })
-        .render("#paypal" + this.eventProps._id);
-      //This function displays Smart Payment Buttons on your web page.
-    },
-    methods: {
-      register() {
-        this.visibleReg = true;
-      },
-
-      showPay() {
-        this.visiblePay = true;
-      },
-
-      deleteEvent() {
-        this.$store.dispatch("deleteEvent", this.eventProps);
-      },
-      closeButton() {
-        this.visibleReg = false;
-      },
-      closeAll() {
-        this.visiblePay = false;
-        this.visibleReg = false;
+import EventEditModal from "../components/editEventModal";
+export default {
+  name: "EventCards",
+  props: ["eventProps"],
+  data() {
+    return {
+      visibleReg: false,
+      visiblePay: false,
+      athlete: {
+        eventTitle: this.eventProps.title,
+        eventDate: this.eventProps.eventDate
       }
+    };
+  },
+  mounted() {
+    paypal
+      .Buttons({
+        createOrder: function(data, actions) {
+          // This function sets up the details of the transaction, including the amount and line item details.
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: 20
+                }
+              }
+            ]
+          });
+        },
+        onApprove: function(data, actions) {
+          // This function captures the funds from the transaction.
+          return actions.order.capture().then(function(details) {
+            // This function shows a transaction success message to your buyer.
+            alert("Transaction completed by " + details.payer.name.given_name);
+          });
+        }
+      })
+      .render("#paypal" + this.eventProps._id);
+    //This function displays Smart Payment Buttons on your web page.
+  },
+  methods: {
+    register() {
+      this.visibleReg = true;
     },
-    components: {
-      EventEditModal
+
+    showPay() {
+      this.visiblePay = true;
+    },
+
+    deleteEvent() {
+      this.$store.dispatch("deleteEvent", this.eventProps);
+    },
+    closeButton() {
+      this.visibleReg = false;
+    },
+    closeAll() {
+      this.visiblePay = false;
+      this.visibleReg = false;
     }
-  };
+  },
+  components: {
+    EventEditModal
+  }
+};
 </script>
 <style>
-  .cardStyle {
-    height: 40vh;
-    margin-bottom: 2vh;
-    display: flex;
-  }
+.cardStyle {
+  height: 40vh;
+  margin-bottom: 2vh;
+  display: flex;
+}
 
-  .imgStyle {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(255, 255, 255, 0.644);
-    border-top-left-radius: 20px;
-    border-bottom-left-radius: 20px;
-  }
+.imgStyle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.644);
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+}
 
-  .eventInfo {
-    display: grid;
-    align-content: center;
-    background-color: rgba(255, 255, 255, 0.644);
-    border-top-right-radius: 20px;
-    border-bottom-right-radius: 20px;
-    text-shadow: 1px 1px white;
-    overflow-y: hidden;
-  }
+.eventInfo {
+  display: grid;
+  align-content: center;
+  background-color: rgba(255, 255, 255, 0.644);
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  text-shadow: 1px 1px white;
+  overflow-y: hidden;
+}
 
-  .registerStyle {
-    padding: 20px;
-    display: grid;
-    align-content: center;
-    background-color: rgba(255, 255, 255, 0.644);
-    border-radius: 20px;
-    text-shadow: 1px 1px white;
-  }
+.registerStyle {
+  padding: 20px;
+  display: grid;
+  align-content: center;
+  background-color: rgba(255, 255, 255, 0.644);
+  border-radius: 20px;
+  text-shadow: 1px 1px white;
+}
 
-  .payStyle {
-    align-content: center;
-    background-color: rgba(255, 255, 255, 0.644);
-    border-radius: 20px;
-  }
+.payStyle {
+  align-content: center;
+  background-color: rgba(255, 255, 255, 0.644);
+  border-radius: 20px;
+}
 
-  img {
-    height: 90%;
-    width: 90%;
-    border-radius: 20px;
-  }
+img {
+  height: 90%;
+  width: 90%;
+  border-radius: 20px;
+}
 
-  .webLink {
-    color: black;
-  }
+.webLink {
+  color: black;
+}
 
-  .btn.fab {
-    padding: 1rem;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.btn.fab {
+  padding: 1rem;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .registerButton {
-    position: absolute;
-    right: 1rem;
-    top: 11rem;
-  }
+.registerButton {
+  position: absolute;
+  right: 1rem;
+  top: 42%;
+}
 
-  .closeButton {
-    position: absolute;
-    right: 1rem;
-    top: 1rem;
-  }
+.closeButton {
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+}
 
-  .buttonRow {
-    display: flex;
-    justify-content: space-evenly;
-  }
+.buttonRow {
+  display: flex;
+  justify-content: space-evenly;
+}
 
-  .btnSm {
-    border-radius: 50%;
-    border-color: black;
-    border-style: solid;
-  }
+.btnSm {
+  border-radius: 50%;
+  border-color: black;
+  border-style: solid;
+}
 </style>
