c8057b0a4ba8ce36ad983fcab72fd0f880b63164
Rolland Campbell <rollandcampbell@msn.com>
2020-04-13 17:52:58 -0600

    adj practice page

--- clientsrc/src/views/Practices.vue
+++ clientsrc/src/views/Practices.vue
@@ -2,21 +2,22 @@
   <div class="practices">
     <div class="row titleRow">
       <h1 class="text-center">Heavy Events Practice</h1>
-      <div class="col-10 offset-1 col-md-6">
+      <div class="col-10 offset-1">
         <img class="locPic" src="../assets/practiceLocation.jpg" alt="practice location" />
       </div>
     </div>
     <div class="row textRow">
       <div class="col-8">
-        <p>We hold practice at McDevitt's Park on the corner of McMillan Rd and Eagle Rd. Practices are organized with trainers on site to help you start your experience, and to help seasoned athletes become better. We try and run practices every weekend from April - October. Practice will not be held on event weekends. If you are interested in starting or would like to know when practices will be. Register for our group notifications to recieve text messages.</p>
+        <p>We hold practice at McDevitt's Park on the corner of McMillan Rd and Eagle Rd. Practices are organized with
+          trainers on site to help you start your experience, and to help seasoned athletes become better. We try and
+          run practices every weekend from April - October. Practice will not be held on event weekends. If you are
+          interested in starting or would like to know when practices will be. Register for our group notifications to
+          recieve text messages.</p>
       </div>
     </div>
     <div class="row buttonRow">
-      <button
-        class="btn btn-warning"
-        data-toggle="modal"
-        data-target="#textModal"
-      >Add me to the text message group</button>
+      <button class="btn btn-warning" data-toggle="modal" data-target="#textModal">Add me to the text message
+        group</button>
       <TextModal />
     </div>
   </div>
@@ -24,65 +25,65 @@
 
 
 <script>
-import TextModal from "../components/textModal";
-export default {
-  name: "Practices",
-  data() {
-    return {};
-  },
-  computed: {},
-  methods: {},
-  components: {
-    TextModal
-  }
-};
+  import TextModal from "../components/textModal";
+  export default {
+    name: "Practices",
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {},
+    components: {
+      TextModal
+    }
+  };
 </script>
 
 
 <style scoped>
-.practices {
-  height: 90vh;
-  background-image: url("../assets/practicebg.jpg");
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 100% 100%;
-  overflow: hidden;
-  overflow-y: auto;
-}
+  .practices {
+    height: 90vh;
+    background-image: url("../assets/practicebg.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 100% 100%;
+    overflow: hidden;
+    overflow-y: auto;
+  }
 
-.titleRow {
-  display: grid;
-  justify-content: center;
-  color: white;
-  text-shadow: 1px 1px black;
-  padding: 10px;
-}
-
-.locPic {
-  height: 40vh;
-}
-
-@media only screen and (max-width: 600px) {
-  .locPic {
-    width: 100%;
-    height: 30vh;
+  .titleRow {
+    display: grid;
+    justify-content: center;
+    color: white;
+    text-shadow: 1px 1px black;
     padding: 10px;
   }
-}
 
-p {
-  color: white;
-  text-shadow: 1px 1px black;
-}
+  .locPic {
+    height: 40vh;
+  }
 
-.textRow {
-  justify-content: center;
-  margin-top: 5vh;
-  font-size: x-large;
-}
+  @media only screen and (max-width: 600px) {
+    .locPic {
+      width: 100%;
+      height: 30vh;
+      padding: 10px;
+    }
+  }
 
-.buttonRow {
-  justify-content: center;
-  height: 8vh;
-}
+  p {
+    color: white;
+    text-shadow: 1px 1px black;
+  }
+
+  .textRow {
+    justify-content: center;
+    margin-top: 5vh;
+    font-size: x-large;
+  }
+
+  .buttonRow {
+    justify-content: center;
+    height: 8vh;
+  }
 </style>
