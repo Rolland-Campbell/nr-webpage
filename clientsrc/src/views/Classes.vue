87dad66ca7669daa1bb2c876fe6cba3197000a14
Rolland Campbell <rollandcampbell@msn.com>
2020-04-22 12:39:18 -0600

    class table done

--- clientsrc/src/views/Classes.vue
+++ clientsrc/src/views/Classes.vue
@@ -1,6 +1,240 @@
 <template>
   <div class="classesStyle">
-
+    <div class="row titleText">
+      <p>Heavy Athletics Athlete Class Guide</p>
+      <p class="lowerTitle">These values are based on the average throws for each class over a 1 year period.</p>
+      <p class="lowerTitle">Classes not displayed are Master's Classes, Men/Women's Novice, Women's C</p>
+    </div>
+    <div class="row tableRow ml-4 mr-4">
+      <table class="table">
+        <thead>
+          <tr>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row">Men's Pro</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>38' - 44'</td>
+            <td>48' - 55'</td>
+            <td>37' - 43'</td>
+            <td>75' - 85'</td>
+            <td>95' - 105'</td>
+            <td>120' - 140'</td>
+            <td>23'</td>
+            <td>13'</td>
+          </tr>
+          <tr>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+          </tr>
+          <tr>
+            <td scope="row">Women's Pro</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>30' - 35'</td>
+            <td>36' - 43'</td>
+            <td>38' - 45'</td>
+            <td>65' - 80'</td>
+            <td>75' - 85'</td>
+            <td>90' - 105'</td>
+            <td>27'</td>
+            <td>15'</td>
+          </tr>
+          <tr>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+          </tr>
+          <tr>
+            <td scope="row">Men's A</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>32' - 38'</td>
+            <td>40' - 45'</td>
+            <td>30' - 36'</td>
+            <td>60' - 70'</td>
+            <td>85' - 100'</td>
+            <td>100' - 115'</td>
+            <td>28'</td>
+            <td>13'</td>
+          </tr>
+          <tr>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+          </tr>
+          <tr>
+            <td scope="row">Women's A</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>25' - 30'</td>
+            <td>32' - 36'</td>
+            <td>34' - 38'</td>
+            <td>58' - 65'</td>
+            <td>60' - 70'</td>
+            <td>75' - 85'</td>
+            <td>23'</td>
+            <td>13'</td>
+          </tr>
+          <tr>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+          </tr>
+          <tr>
+            <td scope="row">Men's B</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>28' - 32'</td>
+            <td>35' - 38'</td>
+            <td>26' - 29'</td>
+            <td>50' - 60'</td>
+            <td>70' - 80'</td>
+            <td>90' - 100'</td>
+            <td>24'</td>
+            <td>12'</td>
+          </tr>
+          <tr>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+          </tr>
+          <tr>
+            <td scope="row">Women's B</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>21' - 25'</td>
+            <td>27' - 32'</td>
+            <td>32' - 36'</td>
+            <td>50' - 58'</td>
+            <td>55' - 60'</td>
+            <td>65' - 75'</td>
+            <td>20'</td>
+            <td>12'</td>
+          </tr>
+          <tr>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+            <td class="spaceRow"></td>
+          </tr>
+          <tr>
+            <td scope="row">Men's C</td>
+            <td>Braemar</td>
+            <td>Open Stone</td>
+            <td>Heavy W4D</td>
+            <td>Light W4D</td>
+            <td>Heavy Hammer</td>
+            <td>Light Hammer</td>
+            <td>Sheaf</td>
+            <td>WOB</td>
+          </tr>
+          <tr>
+            <td scope="row">Target Range</td>
+            <td>24' - 28'</td>
+            <td>31' - 36'</td>
+            <td>21' - 26'</td>
+            <td>42' - 50'</td>
+            <td>60' - 70'</td>
+            <td>80' - 90'</td>
+            <td>20'</td>
+            <td>11'</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script>
@@ -15,11 +249,64 @@
 <style>
   .classesStyle {
     height: 90vh;
-    background-image: url("../assets/contactbg.jpg");
+    background-image: url("../assets/classesbg.jpg");
     background-repeat: no-repeat;
     background-position: center;
     background-size: 100% 100%;
     overflow: hidden;
     overflow-y: auto;
+    text-align: center;
+  }
+
+  .spaceRow {
+    background-color: white;
+  }
+
+  .titleText {
+    height: 11vh;
+    justify-content: center;
+    align-content: center;
+    color: white;
+    text-shadow: 1px 1px black;
+    font-size: larger;
+    display: block;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .titleText {
+      height: 20vh;
+      justify-content: center;
+      align-content: center;
+      color: white;
+      text-shadow: 1px 1px black;
+      font-size: larger;
+      display: block;
+      padding-left: 5vw;
+      padding-right: 5vw;
+    }
+  }
+
+  .lowerTitle {
+    font-size: small;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .tableRow {
+      overflow-x: scroll;
+      padding: 0;
+    }
+  }
+
+  td {
+    color: white;
+    text-shadow: 1px 1px black;
+  }
+
+  @media only screen and (max-width: 600px) {
+    td {
+      color: white;
+      text-shadow: 1px 1px black;
+      font-size: small;
+    }
   }
 </style>
